3d0185678682077bd25aac67d63d9d0ddde3bb16
Nathcorp123 <84910300+Nathcorp123@users.noreply.github.com>
2023-07-19 16:52:23 +0530

    first code

---
+++ Steps/steps.cs
@@ -0,0 +1,40 @@
+﻿using finalspecflow.POM;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace finalspecflow.Steps
+{
+    [Binding]
+    public class ToTestLoginFunctionalityForOrangeHRmApplicationSteps
+    {
+        pom Call = new pom();
+
+        [Given(@"Launch the browser and open the url")]
+        public void GivenLaunchTheBrowserAndOpenTheUrl()
+        {
+            Call.clickLogin();
+            Thread.Sleep(3000);
+        }
+        
+        [Given(@"Enter the valid userid (.*) and password(.*)")]
+        public void GivenEnterTheValidUseridAndPassword(string emailid, string password)
+        {
+            Call.enterEmail(emailid, password);
+            Thread.Sleep(3000);
+        }
+        
+        [When(@"Click on login button")]
+        public void WhenClickOnLoginButton()
+        {
+            Call.login();
+            Thread.Sleep(3000);
+        }
+        
+        [Then(@"User should be sucessfully redirected to home page of application")]
+        public void ThenUserShouldBeSucessfullyRedirectedToHomePageOfApplication()
+        {
+            Console.WriteLine("Welcome to home page");
+        }
+    }
+}
